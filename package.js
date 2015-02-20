33797a472b592ebc4ae30301e9eb3347c4ee63ba
Patrick Scott <patrick.lee.scott@gmail.com>
2015-02-19 22:35:27 -0500

    upgrade meteor and collection2

--- package.js
+++ package.js
@@ -1,15 +1,15 @@
 Package.describe({
   name: "patrickleet:tags",
   summary: "Add groups of tags to selected collections",
-  version: "1.1.2",
+  version: "1.1.3",
   git: "https://github.com/patrickleet/tags"
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('METEOR@1.0.2.1');
+  api.versionsFrom('METEOR@1.0.3.1');
   
   api.use(['livedata', 'mongo-livedata'], ['client', 'server']);
-  api.use('aldeed:collection2@2.3.1', ['client', 'server'], {weak: true});
+  api.use('aldeed:collection2@2.3.2', ['client', 'server'], {weak: true});
   api.use('aldeed:simple-schema@1.3.0', ['client', 'server'], {weak: true});
   
   api.addFiles('patrickleet:tags.js', ['client', 'server']);
