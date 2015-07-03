6f14295c39d7e00ff8e5ab7363ed7da352db11db
Jamie Collinson <jamiecollinson@gmail.com>
2015-07-03 16:39:15 +0100

    Fix nRefs count on removeTag, also remove tags with nRefs === 0

--- package.js
+++ package.js
@@ -7,11 +7,11 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@1.0.3.1');
-  
+
   api.use(['livedata', 'mongo-livedata'], ['client', 'server']);
   api.use('aldeed:collection2@2.3.2', ['client', 'server'], {weak: true});
   api.use('aldeed:simple-schema@1.3.0', ['client', 'server'], {weak: true});
-  
+
   api.addFiles('patrickleet-tags.js', ['client', 'server']);
 
   if (api.export !== undefined) {
@@ -21,6 +21,6 @@ Package.onUse(function(api) {
 
 Package.onTest(function(api) {
   api.use('tinytest');
-  api.use('patrickleet-tags');
+  api.use('patrickleet:tags');
   api.addFiles('patrickleet-tags-tests.js');
 });
